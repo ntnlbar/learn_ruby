19d7b90fe2ced5bffa78cc4b3f5c2e50e2bed9f1
Nethanel Bar <ntnlbar@gmail.com>
2017-06-02 00:41:12 +0300

    Created simon_says.rb

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,24 @@
 #write your code here
+def echo string
+  string
+end
+
+def shout string
+  string.upcase
+end
+
+def repeat string,times=2
+  ((string + ' ') * times).strip
+end
+
+def start_of_word string, pos
+  string[0...pos]
+end
+
+def first_word string
+  string.split()[0]
+end
+
+def titleize string
+  string.titleize
+end
