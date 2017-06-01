f58f2e3277b2be07d1f58ef09a3faef1e092305b
Nethanel Bar <ntnlbar@gmail.com>
2017-06-01 18:26:30 +0300

    Created calculator.rb and changed calculator_spec.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
 #write your code here
+def add a,b
+  a + b
+end
+
+def subtract a,b
+  a - b
+end
+
+def sum arr
+  s = 0
+  arr.each { |i| s += i }
+  s
+end
+
+def multiply arr
+  mul = 1
+  arr.each {|i| mul *= i}
+  mul
+end
+
+def power a,b
+  s = 1
+  b.times {s *= a}
+  s
+end
+
+def fact num
+  if num <= 1
+    1
+  else
+    num * fact(num-1)
+  end
+end
+
+puts fact(3)
