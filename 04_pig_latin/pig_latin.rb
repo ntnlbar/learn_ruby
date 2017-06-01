744586be6b2c79d558191a21b49b7e762f2d7bb4
Nethanel Bar <ntnlbar@gmail.com>
2017-06-02 01:08:24 +0300

    Created pig_latin.rb

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,25 @@
 #write your code here
+
+$vowel = 'a e u o i'.split()
+
+def translate string
+  res = ''
+  string = string.split()
+
+  string.each do |word|
+    res += word_translate(word) + ' '
+  end
+  res.strip
+end
+
+def word_translate word
+
+  first_chr = word[0]
+  if $vowel.include? first_chr
+    word + 'ay'
+  elsif word[0..1] == 'qu'
+    word_translate(word[2..-1] + word[0..1])
+  else
+    word_translate(word[1..-1] + word[0])
+  end
+end
