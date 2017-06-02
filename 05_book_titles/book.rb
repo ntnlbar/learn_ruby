9403716598bcda0768cc956e36bded23ae03cfea
Nethanel Bar <ntnlbar@gmail.com>
2017-06-02 10:02:10 +0300

    Created book.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+  attr_accessor :title
+  @@not_cap = ['and','a','an','in','of','the']
+  def title=title
+    words = title.split()
+    @title = words[0].capitalize + ' '
+    words[1..-1].each do |word|
+      if @@not_cap.include? word
+        @title += word + ' '
+      else
+        @title += word.capitalize + ' '
+      end
+    end
+    @title.strip!
+  end
 end
