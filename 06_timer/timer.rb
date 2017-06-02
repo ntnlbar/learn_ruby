22f8f990f39dd1fba9a7a2c0e1dc3e8575e149d1
Nethanel Bar <ntnlbar@gmail.com>
2017-06-02 15:44:03 +0300

    Created timer.rb and finshed the challenge!

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,13 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+  def time_string
+    hour = (@seconds / 3600).to_s.rjust(2,'0')
+    min = (( @seconds % 3600 ) / 60).to_s.rjust(2,'0')
+    sec = (( @seconds % 3600 ) % 60).to_s.rjust(2,'0')
+    "#{hour}:#{min}:#{sec}"
+  end
 end
